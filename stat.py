43cb389e72c8c85770c598eb97f8d933d6a25f9e
iBug <7273074+iBug@users.noreply.github.com>
2018-03-25 15:04:18 +0800

    .

--- stat.py
+++ stat.py
@@ -1,6 +1,7 @@
 # Some statistics functions
 
-from math import sqrt
+def sqrt(x):
+    return pow(x, 0.5)
 
 def avg(l):
     try:
@@ -18,3 +19,18 @@ def ubstddev(l, a=None):
 
 def bstddev(l, a=None):
     return stddev(l, a=a, unbiased=0)
+
+class linreg:
+    @staticmethod
+    def b(x, y):
+        n, ax, ay = len(x), avg(x), avg(y)
+        return (sum([a * b for a, b in zip(x, y)]) - n * ax * ay) / (sum([i**2 for i in x]) - n * ax**2)
+
+    @staticmethod
+    def a(x, y):
+        return avg(y) - b(x, y) * avg(x)
+
+    @staticmethod
+    def re(x, y):
+        n, ax, ay = len(x), avg(x), avg(y)
+        return (sum([a * b for a, b in zip(x, y)]) - n * ax * ay) / pow((sum([i**2 for i in x]) - n * ax**2)*(sum([i**2 for i in y]) - n * ay**2), 0.5)
